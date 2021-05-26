bc087c90a6c57ba2da2a0d9115f1b46604346d08
newrepsanam <smaharjan@newrepublique.com>
2021-05-26 12:20:44 +1000

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,62 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import jszip from 'jszip';
+import { saveAs } from 'file-saver';
+import JSZipUtils from 'jszip-utils';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+
+var zip = new jszip();
+var count = 0;
+var zipFilename = "brochure.zip";
+var urls = [
+  '/images/a.jpg',
+  '/images/b.jpg',
+  '/images/c.jpg'
+];
+
+
+urls.forEach(async function (url){
+  const urlArr = url.split('/');
+  const filename = urlArr[urlArr.length - 1];
+  try {
+    const file = await JSZipUtils.getBinaryContent(url)
+    zip.file(filename, file, { binary: true});
+    count++;
+    if(count === urls.length) {
+      zip.generateAsync({type:'blob'}).then(function(content) {
+        saveAs(content, zipFilename);
+      });
+    }
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React aggggg
         </a>
-      </header>
-    </div>
-  );
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
