0097917a119feb38c8b77979aabb5b4005769f9a
newrepsanam <smaharjan@newrepublique.com>
2021-05-26 15:33:53 +1000

    added pdfs

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import logo from './logo.svg';
 import './App.css';
 import React from 'react';
 import jszip from 'jszip';
@@ -6,53 +5,48 @@ import { saveAs } from 'file-saver';
 import JSZipUtils from 'jszip-utils';
 
 
-var zip = new jszip();
-var count = 0;
-var zipFilename = "brochure.zip";
-var urls = [
-  '/images/a.jpg',
-  '/images/b.jpg',
-  '/images/c.jpg'
-];
-
-
-urls.forEach(async function (url){
-  const urlArr = url.split('/');
-  const filename = urlArr[urlArr.length - 1];
-  try {
-    const file = await JSZipUtils.getBinaryContent(url)
-    zip.file(filename, file, { binary: true});
-    count++;
-    if(count === urls.length) {
-      zip.generateAsync({type:'blob'}).then(function(content) {
-        saveAs(content, zipFilename);
-      });
-    }
-  } catch (err) {
-    console.log(err);
-  }
-});
-
-
-
 class App extends React.Component {
+
+  brochureDownload = () => {
+    var zip = new jszip();
+    var count = 0;
+    var zipFilename = "brochure.zip";
+    var urls = [
+      '/pdfs/Subaru-XV-brochure.pdf',
+      '/pdfs/Subaru-hybrid-brochure.pdf',
+      '/pdfs/Subaru-WRX-STI-brochure.pdf'
+    ];
+
+
+    urls.forEach(async function (url) {
+      const urlArr = url.split('/');
+      const filename = urlArr[urlArr.length - 1];
+      try {
+        const file = await JSZipUtils.getBinaryContent(url)
+        zip.file(filename, file, { binary: true });
+        count++;
+        if (count === urls.length) {
+          zip.generateAsync({ type: 'blob' }).then(function (content) {
+            saveAs(content, zipFilename);
+          });
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    });
+
+  }
+
+  handleDownloadBrochure = () => {
+    this.brochureDownload();
+  }
 
   render() {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-        </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React aggggg
-        </a>
+
+          <button className="download-brochure" onClick={this.handleDownloadBrochure}>Download</button>
         </header>
       </div>
     );
